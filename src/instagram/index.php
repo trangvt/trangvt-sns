bc575eac656110806fa734d9dfaa59df90ce1651
trangvt <trangvt.khtn@gmail.com>
2017-08-14 11:45:05 +0700

    Insta - multiple image

--- src/instagram/index.php
+++ src/instagram/index.php
@@ -2,17 +2,14 @@
 session_start();
 require_once(dirname(dirname(__FILE__)).'/config.php');
 
-// Upload Photo
-$insta = new InstagramUpload();
-$insta->Login(INSTA_USER, INSTA_PASS);
-
 $caption = $_SESSION['caption'];
 $photos_arr = $_SESSION['photos_arr'];
 
 foreach ($photos_arr as $key => $photo) {
 	try {
+		$insta = new InstagramUpload();
+		$insta->Login(INSTA_USER, INSTA_PASS);
 		$insta->UploadPhoto($photo, $caption);
-		echo $key;
 	} catch (Exception $e) {
 		echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
